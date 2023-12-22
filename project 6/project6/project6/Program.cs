a74e374650062ad91bec06985274ac64429e6cd6
unknown <anarpi@code.edu.az>
2023-12-22 18:12:15 +0400

    refresh

---
+++ project 6/project6/project6/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+class Program
+{
+    static void Main()
+    {
+
+        //Verilmiş təhsil növü dəyərinə əsasən o təhsilin saatını göstərən proqram
+        //Əgər verilmiş dəyər "programming"dirsə console-da 400 saat, "design"dırsa 250 saat,
+        //"system"dirsə 200 saat, heç biri deyilsə "təhsil novu yanlisdir" yazılmalıdır console-da.
+
+        Console.WriteLine("tehsil novu daxil edin"); 
+        
+        string nov =Console.ReadLine();
+
+
+        if (nov == "programming")
+        {
+            Console.WriteLine("400 saat");
+
+        }
+        
+        
+
+        if (nov == "design")
+        {
+                Console.WriteLine("250 saat");
+        }
+
+        
+
+        if (nov == "system")
+        {
+            Console.WriteLine("200 saat");
+
+
+        }
+
+
+
+        if (nov != "system" && nov != "programming" && nov != "design")
+        {
+            Console.WriteLine("tehsil novu yanlisdir");
+        }
+        
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
